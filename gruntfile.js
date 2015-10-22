f20376eb1fcd623527572ad33ca8fce29c19c24a
kelter <miguel.aka.kelter@gmail.com>
2015-10-22 15:24:19 +0100

    init

---
+++ gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            sitecss: {
+                options: {
+                    banner: '/* My minified css file */'
+                },
+                files: {
+                    'css/bundle/os-mt-core.min.css': [
+                        'css/bootstrap.css'
+                    ],
+                    'css/bundle/os-mt-kbs.min.css': [
+                        'css/evaluation-kb.css',
+                        'css/support-kb.css'
+                    ]
+                }
+            }
+        }/*,
+        uglify: {
+            options: {
+                compress: true
+            },
+            build: {
+                files: {
+                    'js/bundle/os-portal.js': [
+                        'js/src/portal-common.js',
+                        'js/src/bootstrap.js'
+                    ],
+                    'js/bundle/os-portal-extra.js': [
+
+
+                    ]
+                }
+            }
+        }*/
+    });
+    // Default task.
+    //grunt.registerTask('default', ['cssmin', 'uglify']);
+    grunt.registerTask('default', ['cssmin']);
+};
