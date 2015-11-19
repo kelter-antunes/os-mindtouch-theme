f695331c4f423921873b7caae03f5ef1e2b028d0
kelter <miguel.aka.kelter@gmail.com>
2015-11-19 17:49:47 +0000

    popup

--- gruntfile.js
+++ gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
                     banner: '/* My minified css file */'
                 },
                 files: {
-                    'css/bundle/os-mt-popup.css': [
+                    'css/bundle/os-mt-popup.min.css': [
                         'css/normalize.css',
                         'css/popups.css'
                     ],
